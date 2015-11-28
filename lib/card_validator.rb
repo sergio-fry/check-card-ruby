e3d8a21d53c833c0b165c8553b1d4b5a144a937f
Sergei O. Udalov <sergei.udalov@gmail.com>
2015-11-28 22:33:54 +0300

    start_with? refactoring

--- lib/card_validator.rb
+++ lib/card_validator.rb
@@ -2,13 +2,13 @@ require "card_validator/version"
 
 module CardValidator
   def detect_type(card)
-    if [34, 35].include?(card[0..1].to_i) && card.size == 15
+    if card.start_with?("34", "35") && card.size == 15
       "AMEX"
-    elsif card[0..3].to_i == 6011 && card.size == 16
+    elsif card.start_with?("6011") && card.size == 16
       "Discover"
-    elsif (51..55).include?(card[0..1].to_i) && card.size == 16
+    elsif card.start_with?(*(51..55).map(&:to_s)) && card.size == 16
       "MasterCard"
-    elsif card[0] == "4" && [13, 16].include?(card.size)
+    elsif card.start_with?("4") && [13, 16].include?(card.size)
       "Visa"
     else
       "Unknown"
