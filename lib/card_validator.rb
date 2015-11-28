d3436da6a9b90d584b7b66ed33b7c4c53de93866
Sergei O. Udalov <sergei.udalov@gmail.com>
2015-11-28 21:38:42 +0300

    Discovery

--- lib/card_validator.rb
+++ lib/card_validator.rb
@@ -4,6 +4,10 @@ module CardValidator
   def detect_type(card)
     if [34, 35].include?(card[0..1].to_i) && card.size == 15
       "AMEX"
+    elsif card[0..3].to_i == 6011 && card.size == 16
+      "Discover"
+    else
+      "Unknown"
     end
   end
 end
--- spec/card_validator_spec.rb
+++ spec/card_validator_spec.rb
@@ -5,18 +5,36 @@ describe CardValidator do
   include CardValidator
 
   describe "#detect_type" do
-    it "should detect AMEX" do
-      expect(detect_type("341111111111111")).to eq("AMEX")
-      expect(detect_type("351111111111111")).to eq("AMEX")
+    describe "AMEX" do
+      it "should detect card" do
+        expect(detect_type("341111111111111")).to eq("AMEX")
+        expect(detect_type("351111111111111")).to eq("AMEX")
+      end
+
+      it "should reject card when length != 15" do
+        expect(detect_type("34111111111111")).to eq("Unknown")
+        expect(detect_type("3511111111111111")).to eq("Unknown")
+      end
+
+      it "should reject card when start not from 34/35" do
+        expect(detect_type("331111111111111")).to eq("Unknown")
+      end
     end
 
-    it "should reject AMEX when length != 15" do
-      expect(detect_type("34111111111111")).not_to eq("AMEX")
-      expect(detect_type("3511111111111111")).not_to eq("AMEX")
+    describe "Discover" do
+      it "should detect card" do
+        expect(detect_type("6011111111111111")).to eq("Discover")
+      end
+
+      it "should reject card when length != 16" do
+        expect(detect_type("601111111111111")).to eq("Unknown")
+        expect(detect_type("60111111111111111")).to eq("Unknown")
+      end
+
+      it "should reject card when start not from 34/35" do
+        expect(detect_type("5011111111111111")).to eq("Unknown")
+      end
     end
 
-    it "should reject AMEX when start not from 34/35" do
-      expect(detect_type("331111111111111")).not_to eq("AMEX")
-    end
   end
 end
